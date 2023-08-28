3e851247675466e5d46bb78cfb69879da0dfc552
Simon Guillorit <simon.guillorit@gmail.com>
2023-08-28 16:11:38 +0200

    Update RELEVANT_FILES

--- app/params.py
+++ app/params.py
@@ -1,15 +1,20 @@
 BDNB_FILES_URI = "https://open-data.s3.fr-par.scw.cloud/bdnb_millesime_2022-10-d/"
-UNRELEVANT_TABLES = [
-    "proprietaire",
-    "parcelle",
-    "dvf_open",
-    "merimee",
+UNRELEVANT_TABLES_SIMON = [
     "qpv",
-    "dvf_open_statistique",
-    "dvf_open_representatif",
-    "argiles",
-    "geospx",
     "radon",
-    "osm_building",
-    "hthd",
+    "proprietaire",
+    "iris_simulations_valeur_verte",
+    "local",
+    "local_simulation_dpe",
+    "parcelle_unifiee",
+    "parcelle_unifiee_metrique",
+    "bdnb_version",
+    "synthese_systeme_energetique_logement",
+    "synthese_periode_construction",
+    "synthese_enveloppe",
+    "simulations_valeur_verte",
+    "simulations_dvf",
+    "simulations_dpe",
 ]
+RELEVANT_FILES_SIMON = ["batiment_groupe_rnc", "dpe_logement"]
+UNRELEVANT_TABLES = []
