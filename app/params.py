7c5cde1ea55ea623a09f64e06035a971b7aaffc1
antonindavid <antonin.david@edu.escp.eu>
2023-08-28 16:23:24 +0200

    Finished unrelevant tables Antonin

--- app/params.py
+++ app/params.py
@@ -1,15 +1,16 @@
 BDNB_FILES_URI = "https://open-data.s3.fr-par.scw.cloud/bdnb_millesime_2022-10-d/"
-UNRELEVANT_TABLES = [
-    # "proprietaire",
-    # "parcelle",
-    # "dvf_open",
-    # "merimee",
-    # "qpv",
-    # "dvf_open_statistique",
-    # "dvf_open_representatif",
-    # "argiles",
-    # "geospx",
-    # "radon",
-    # "osm_building",
-    # "hthd",
+UNRELEVANT_TABLES_ANTONIN = [
+     "batiment_groupe_dvf",
+     "batiment_groupe_dvf_logtype",
+     "batiment_groupe_dvf_open_representatif",
+     "batiment_groupe_dvf_open_statistique",
+     "batiment_groupe_energie",
+     "batiment_groupe_hthd",
+     "batiment_groupe_enepdl",
+     "batiment_groupe_enethermo",
+     "batiment_groupe_exposition_bruit_logement_social	",
+     "batiment_groupe_hthd",
+     "batiment_groupe_merimee",
+     "batiment_groupe_pie",
+     "batiment_groupe_proprietaire"
 ]
