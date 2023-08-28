64d41c10f5610e228c1abdac685e4eb88f798a11
Thomas RIVIERE <riviere-thomas@outlook.fr>
2023-08-28 18:04:54 +0200

    liste table important / pas importante thomas

--- app/params.py
+++ app/params.py
@@ -18,3 +18,26 @@ UNRELEVANT_TABLES_SIMON = [
 ]
 RELEVANT_FILES_SIMON = ["batiment_groupe_rnc", "dpe_logement"]
 UNRELEVANT_TABLES = []
+
+UNRELEVANT_TABLES_THOMAS = [
+    "adresse", #sauf si cle_interop_adr nécessaire
+    "adresse_metrique",
+    "batiment", #inaccessible
+    "batiment_groupe_adresse",
+    "batiment_groupe_bdtopo_equ", # détail gare métro etc ..
+    "batiment_groupe_bdtopo_zoac", # détail lieu public
+    "batiment_groupe_bpe", #détail activité au sein d'un batiment (psy, supermarché etc..)
+    "batiment_groupe_dle_elec_2020",#compris dans dle_elec_multimillesime
+    "batiment_groupe_dle_gaz_2020",#compris dans dle_gaz_multimillesime
+    "batiment_groupe_dle_reseaux_2020"#compris dans dle_reseaux_multimillesime
+]
+
+RELEVANT_FILES_THOMAS= [
+    "batiment_construction",# batiment_groupe_id,batiment_construction_id, geometry, alitude, hauteur
+    "batiment_groupe",#batiment_groupe_id , s_geom_groupe, geom_groupe
+    "batiment_groupe_argiles",#batiment_groupe_id, alea  -> exposition aux risques climatiques du batiment
+    "batiment_groupe_bdtopo_bat",#batiment_groupe_id, l_etat -> en projet; en construction, hauteur_mean, altitude_sol_mean
+    "batiment_groupe_dle_elec_multimillesime",#batiment_groupe_id et données conso
+    "batiment_groupe_dle_gaz_multimillesime",#batiment_groupe_id et données conso
+    "batiment_groupe_dle_reseaux_multimillesime"#batiment_groupe_id et données conso
+]
